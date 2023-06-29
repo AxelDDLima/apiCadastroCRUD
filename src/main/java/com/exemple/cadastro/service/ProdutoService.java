978337e2f3fa0b7cc65ae7e172023336e32ac3f4
Axel <axelddlima@hotmail.com>
2023-06-29 14:04:34 -0300

    Implementado o crud de produtos.

--- src/main/java/com/exemple/cadastro/service/ProdutoService.java
+++ src/main/java/com/exemple/cadastro/service/ProdutoService.java
@@ -1,18 +1,13 @@
 package com.exemple.cadastro.service;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.exemple.cadastro.entity.Cliente;
-import com.exemple.cadastro.entity.Endereco;
-import com.exemple.cadastro.repositories.ClienteRepository;
-import com.exemple.cadastro.repositories.EnderecoRepository;
+import com.exemple.cadastro.entity.Produto;
+import com.exemple.cadastro.repositories.ProdutoRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -20,47 +15,38 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class ProdutoService {
 	
-	private ClienteRepository clienteRepository;
-	private EnderecoRepository enderecoRepository;
+	private ProdutoRepository produtoRepository;
 	
 	//Essa é uma maneira de colocar em uma varial statica assim implementando um fake bd
 	//private static Map<Long, Cliente> listaPessoa = new HashMap<>();
 	
-	public Page<Cliente> buscarTodosRegistros(PageRequest page){
-		return clienteRepository.findAll(page);
+	public Page<Produto> buscarTodosRegistros(PageRequest page){
+		return produtoRepository.findAll(page);
 	}
 	
-	public ResponseEntity<Cliente> buscarRegistroId(Long id){
-		if(clienteRepository.existsById(id)) {
-			return ResponseEntity.status(HttpStatus.OK).body(clienteRepository.findById(id).get());
+	public ResponseEntity<Produto> buscarRegistroId(Long id){
+		if(produtoRepository.existsById(id)) {
+			return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.findById(id).get());
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 	
-	public ResponseEntity<Cliente> registrarCliente(Cliente cliente){
-		Set<Endereco> enderecos = cliente.getEnderecos();
-		cliente.setEnderecos(new HashSet<>());
-		
-		Cliente registrarCliente = clienteRepository.save(cliente);
-		
-		for(Endereco endereco : enderecos) {		
-			endereco.setCliente(Cliente.builder().id(cliente.getId()).build());
-			cliente.getEnderecos().add(enderecoRepository.save(endereco));
-		}
-		return ResponseEntity.status(HttpStatus.CREATED).body(registrarCliente);
+	public ResponseEntity<Produto> registrarProduto(Produto produto){
+		Produto registrarProduto = produtoRepository.save(produto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(registrarProduto);
 	}
 	
-	public ResponseEntity<Cliente> editarRegistroId(Long id, Cliente cliente){
-		if(clienteRepository.existsById(id)) {
-			Cliente editarCliente = clienteRepository.save(cliente);
-			return ResponseEntity.status(HttpStatus.OK).body(editarCliente);
+	public ResponseEntity<Produto> editarRegistroId(Long id, Produto produto){
+		if(produtoRepository.existsById(id)) {
+			Produto editarProduto = produtoRepository.save(produto);
+			return ResponseEntity.status(HttpStatus.OK).body(editarProduto);
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
 	
 	public ResponseEntity<String> removerRegistroId(Long id){
-		if(clienteRepository.existsById(id)) {
-			clienteRepository.deleteById(id);
+		if(produtoRepository.existsById(id)) {
+			produtoRepository.deleteById(id);
 			return ResponseEntity.status(HttpStatus.OK).body("Registro Deletado com sucesso");			
 		}
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registro não encontrado");
